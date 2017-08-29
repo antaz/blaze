a4149c6267f65b535a9831c8be018d3d4f9f0b72
antaraz <azriantar@gmail.com>
2017-08-29 16:27:35 +0100

    Adding the isAttacked() function

--- functions.h
+++ functions.h
@@ -17,6 +17,13 @@
 #define RANK120(sq) ((sq) / 10)
 #define FILE120(sq) ((sq) % 10)
 #define SQ64TO120(sq) ((((sq) >> 3)+2)*10 + ((sq) & 7) + 1)
+#define isPawn(piece) (piece == 1 || piece == 7)
+#define isKnight(piece) (piece == 2 || piece == 8)
+#define isBishop(piece) (piece == 3 || piece == 9)
+#define isRook(piece) (piece == 4 || piece == 10)
+#define isQueen(piece) (piece == 5 || piece == 11)
+#define isKing(piece) (piece == 6 || piece == 12)
+#define pieceColor(piece) (piece <= 6 ? 0 : 1)
 
 // ((x/8) + 2)*10 + (x + 7) + 1
 
