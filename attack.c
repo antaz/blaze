e203c7d8dc974aa67c75ac275f47888841e77588
antaraz <azriantar@gmail.com>
2017-08-29 16:29:39 +0100

    writing the isAttacked() function

---
+++ attack.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+#include "definitions.h"
+#include "functions.h"
+
+const int knightDir[8] = { -8, -19, -21, -12, 8, 19, 21, 12 };
+const int rookDir[4] = { -1, -10, 1, 10 };
+const int bishopDir[4] = { -9, -11, 11, 9 };
+const int kingDir[8] = { -1, -10, 1, 10, -9, -11, 11, 9 };
+
+bool isAttacked(int square, Board *board, int turn) {
+	int i, piece, t_square, dir;
+	assert(square != OFFBOARD);
+	assert(turn == WHITE || turn == BLACK);
+
+	// Checking pawn attacks	
+	if(turn == WHITE) {
+		if(board->pieces[square - 11] == wP || board->pieces[square - 9] == wP)
+			return true;
+	} else {
+		if(board->pieces[square + 11] == bP || board->pieces[square + 9] == bP)
+			return true;
+		
+	}
+	// Checking knight attacks
+	
+	for(i = 0; i < 8; i++) {
+		piece = board->pieces[square + knightDir[i]];
+		if(piece != OFFBOARD && isKnight(piece) && pieceColor(piece) == turn)
+			return true;
+	}
+	
+	// Checking Rooks and Queens attacks
+	for(i = 0; i < 4; i++) {
+		dir = rookDir[i]; // dir = -1
+		t_square = square + dir; // t_square = 55 - 1 = 54
+		piece = board->pieces[t_square]; // piece = board->piece[54]
+		while(piece != OFFBOARD) {
+			if(piece != EMPTY) {
+				if((isRook(piece) || isQueen(piece)) && pieceColor(piece) == turn) {
+					return true;
+				}
+				break;
+			}
+			t_square += dir;
+			piece = board->pieces[t_square];
+		}
+	}
+
+	// Checking Rooks and Queens attacks
+	for(i = 0; i < 4; i++) {
+		dir = bishopDir[i];
+		t_square = square + dir;
+		piece = board->pieces[t_square];
+		while(piece != OFFBOARD) {
+			if(piece != EMPTY) {
+				if((isBishop(piece) || isQueen(piece)) && pieceColor(piece) == turn) {
+					return true;
+				}
+				break;
+			}
+			t_square += dir;
+			piece = board->pieces[t_square];
+		}
+	}
+
+	// Checking king attacks 
+	for(i = 0; i < 8; i++) {
+		piece = board->pieces[square + kingDir[i]];
+		if(piece != OFFBOARD && isKing(piece) && pieceColor(piece) == turn) {
+			return true;
+		}
+	}
+
+	return false;
+
+}
