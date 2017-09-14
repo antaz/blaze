5aad45a2f0d5ef8608ff166464c340661ceaa003
antaraz <azriantar@gmail.com>
2017-09-14 21:58:05 +0100

    parsing move from CLI

--- main.c
+++ main.c
@@ -4,13 +4,32 @@
 
 void main() {
 	int i;
-	char *fen = "r3r1k1/p1q1np1p/4bQp1/3p2B1/2p5/2NB4/PPP2PPP/R3R1K1 w - - 0 18";
+	char *fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 	Board board[1];
+	Move move;
 	initZobrist();
 	board->table->table = NULL;
 	MoveList list[1];
 	parseFEN(fen, board);
-	printBoardSAN(board);
 	initTTable(board->table);
+	
+	char input[6];
+	
+	while(1) {
+		printBoardSAN(board);
+		printf("Enter a move > ");
+		fgets(input, 6, stdin);
+		
+		if(input[0] == 'q') {
+			break;
+		} else if(input[0] == 't') {
+			takeMove(board);
+		} else {
+			parseMove(board, input);
+			moveSAN(move);
+		}
+
+		fflush(stdin);
+	}
 	//search(board);
 }
--- move.c
+++ move.c
@@ -3,6 +3,44 @@
 #include "definitions.h"
 #include "functions.h"
 
+void parseMove(Board *board, char *move, Move move) {
+	int from, to;
+	Move m;
+	int moveNum = 0, Move = 0, promoted = EMPTY;
+	MoveList list[1];
+
+	if(move[1] > '8' || move[1] < '1') return 0;
+	if(move[3] > '8' || move[3] < '1') return 0;
+	if(move[0] > 'h' || move[0] < 'a') return 0;
+	if(move[2] > 'h' || move[2] < 'a') return 0;
+	
+	from = FR2SQ120(move[0] - 'a', move[1] - '1');
+	to = FR2SQ120(move[2] - 'a', move[3] - '1');
+	
+	generateMoves(board, list);
+	for(moveNum = 0; moveNum < list->count; moveNum++) {
+		m = list->moves[moveNum];
+		if(m.from == from && m.to == to) {
+			promoted = m.promoted;
+			if(promoted != EMPTY) {
+				if(isRook(promoted) && move[4] == 'r') {
+					return m;
+				} else if(isBishop(promoted) && move[4] == 'b') {
+					return m;
+				} else if(isQueen(promoted) && move[4] == 'q') {
+					return m;
+				} else if(isKnight(promoted) && move[4] == 'n') {
+					return m;
+				}
+				continue;
+			}
+			return 1;
+		}
+	}
+	
+	return 0;
+}
+
 void moveSAN(Move move) {
 	char *pieces = ".pnbrq";
 	int promoted = move.promoted;
