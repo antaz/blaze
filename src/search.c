41dfc8b9b0e7cbc222d6d04441e72ee820e8e8fc
flurt <azriantar@outlook.com>
2019-03-26 22:57:41 +0000

    adding repetition detection

--- src/search.c
+++ src/search.c
@@ -13,48 +13,56 @@ static void checkTime(Search *search) {
 	readInput(search);
 }
 
+static int isRep(Board *board) {
+	for(int i = board->hisPly - board->fiftyMove; i < board->hisPly; ++i) {
+		if(board->zobristHash == board->history[i].zobristHash)
+			return 1;
+	}
+	return 0;
+}
+
 static int qSearch(int alpha, int beta, Board *board, Search *search) {
-	
+
 	int movenum = 0;
 	int legal = 0;
 	int score;
-	
+
 	if((search->nodes & 0x3FF) == 0) {
 		checkTime(search);
 	}
-	
+
 	search->nodes++;
-	
+
 	MoveList list[1];
 	score = evaluate(board);
 
-	if(score >= beta) 
+	if(score >= beta)
 		return beta;
 	if(score > alpha)
 		alpha = score;
-	
+
 	generateCaptures(board, list);
 	score = -2*MATE;
-	
+
 	for(movenum = 0; movenum < list->count; movenum++) {
 		if(!makeMove(board, list->moves[movenum])) continue;
-	
+
 		legal++;
 		score = -qSearch(-beta, -alpha, board, search);
 		takeMove(board);
-		
+
 		if(search->stop) return 0;
-		
+
 		if(score > alpha) {
 			if(score >= beta) return beta;
 			alpha = score;
 		}
 	}
-	return alpha; 
+	return alpha;
 }
 
 static int alphaBeta(int alpha, int beta, int depth, Board *board, Search *search, PV *pv) {
-	
+
 	int bestscore = -2*MATE;
 	int score = -2*MATE;
 	PV childpv;
@@ -62,10 +70,13 @@ static int alphaBeta(int alpha, int beta, int depth, Board *board, Search *searc
 	int movenum = 0;
 	int legal = 0;
 	int check;
-	
+
 	if((search->nodes & 0x3FF) == 0)
 		checkTime(search);
-	
+
+	if(isRep(board)) {
+		return 0;
+	}
 	search->nodes++;
 
 
@@ -74,24 +85,24 @@ static int alphaBeta(int alpha, int beta, int depth, Board *board, Search *searc
 		return qSearch(alpha, beta, board, search);
 	}
 
-	
+
 	MoveList list[1];
 	generateMoves(board, list);
 
 	check = isAttacked(board, board->kingSquare[board->turn], board->turn ^ 1);
-	
+
 	qsort(list->moves, list->count, sizeof(Move), compareMoves);
 
 	for(movenum = 0; movenum < list->count; movenum++) {
-		
+
 		if(!makeMove(board, list->moves[movenum])) {
 			continue;
 		}
-		
+
 		legal++;
 		score = -alphaBeta(-beta, -alpha, depth - 1, board, search, &childpv);
 		takeMove(board);
-		
+
 		if(search->stop) {
 			return 0;
 		}
@@ -106,11 +117,11 @@ static int alphaBeta(int alpha, int beta, int depth, Board *board, Search *searc
 				pv->moves[0] = list->moves[movenum];
 				memcpy(pv->moves + 1, childpv.moves, childpv.count * sizeof(Move));
 				pv->count = childpv.count + 1;
-				
+
 			}
 		}
 	}
-	
+
 	if(legal == 0) {
 		pv->count = 0;
 		if(check) {
@@ -119,7 +130,7 @@ static int alphaBeta(int alpha, int beta, int depth, Board *board, Search *searc
 			return 0;
 		}
 	}
-	
+
 	return alpha;
 
 }
@@ -132,7 +143,7 @@ void search(Board *board, Search *search, PV *pv) {
 	beta = +2*MATE;
 	int current_depth;
 	Move bestmove = {0};
-	
+
 	for(current_depth = 1; current_depth <= search->depth; current_depth++) {
 		score = alphaBeta(alpha, beta, current_depth, board, search, pv);
 		if(search->stop)
