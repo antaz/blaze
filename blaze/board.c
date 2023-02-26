d0121d812464fd15dfa14a73ac90c948eb5dd2b3
blocr <blocr@proton.me>
2023-02-26 20:47:06 +0100

    rudimentary pseudo-legal move generator

--- blaze/board.c
+++ blaze/board.c
@@ -1,5 +1,6 @@
-#include "bitboard.h"
 #include "board.h"
+#include "bitboard.h"
+#include "move.h"
 #include <stdio.h>
 
 void parse(const char *fen, struct board_t *board)
@@ -79,3 +80,122 @@ void parse(const char *fen, struct board_t *board)
         }
 
 }
+
+void generate(const struct board_t *board, uint16_t *moves)
+{
+
+        bool turn = board->turn;
+        uint64_t ours = board->piece[turn];
+        uint64_t theirs = board->piece[!turn];
+        uint64_t all = ours | theirs;
+
+        // Generate pawn moves
+        if (turn) {
+                uint64_t bpawn = board->piece[PAWN] & ours;
+
+                // Single push
+                uint64_t push = (bpawn >> 8) & ~all;
+
+                while (push) {
+                        int to = bsf(push);
+                        *moves++ = move(to + 8, to, QUIET);
+                        push &= push - 1;
+                }
+
+                // Double push
+                uint64_t dpush =
+                    ((bpawn << 8) & ~all) << 8 & 0x000000FF00000000 & ~all;
+
+                while (dpush) {
+                        int to = bsf(dpush);
+                        *moves++ = move(to + 16, to, QUIET);
+                        dpush &= dpush - 1;
+                }
+
+                // Capture
+                while (bpawn) {
+                        int from = bsf(bpawn);
+                        uint64_t target = bpattack(from) & theirs;
+
+                        while (target) {
+                                int to = bsf(target);
+                                *moves++ = move(from, to, CAPTURE);
+                                target &= target - 1;
+                        }
+
+                        bpawn &= bpawn - 1;
+                }
+        } else {
+                uint64_t wpawn = board->piece[PAWN] & ours;
+
+                // Single push
+                uint64_t push = (wpawn << 8) & ~all;
+
+                while (push) {
+                        int to = bsf(push);
+                        *moves++ = move(to - 8, to, QUIET);
+                        push &= push - 1;
+                }
+
+                // Double push
+                uint64_t dpush =
+                    ((wpawn << 8) & ~all) << 8 & 0x00000000FF000000 & ~all;
+
+                while (dpush) {
+                        int to = bsf(dpush);
+                        *moves++ = move(to - 16, to, QUIET);
+                        dpush &= dpush - 1;
+                }
+
+                // Capture
+                while (wpawn) {
+                        int from = bsf(wpawn);
+                        uint64_t target = wpattack(from) & theirs;
+
+                        while (target) {
+                                int to = bsf(target);
+                                *moves++ = move(from, to, CAPTURE);
+                                target &= target - 1;
+                        };
+
+                        wpawn &= wpawn - 1;
+                }
+        }
+        // Generate knight moves
+        uint64_t knight = board->piece[KNIGHT] & ours;
+
+        while (knight) {
+                int from = bsf(knight);
+                uint64_t target = nattack(from) & ~ours;
+
+                while (target) {
+                        int to = bsf(target);
+                        *moves++ = move(from, to, 0);
+                        target &= target - 1;
+                };
+
+                knight &= knight - 1;
+        };
+        // Generate bishop moves
+        // Generate rook moves
+        // Generate queen moves
+        // Generate king moves
+        uint64_t king = board->piece[KING] & ours;
+
+        while (king) {
+                int from = bsf(king);
+                uint64_t target = kattack(from) & ~ours;
+
+                while (target) {
+                        int to = bsf(target);
+                        *moves++ = move(from, to, 0);
+                        target &= target - 1;
+                };
+
+                king &= king - 1;
+        };
+}
+
+void make(struct board_t *board, const uint16_t *move) {}
+
+void unmake(struct board_t *board) {}
--- blaze/board.h
+++ blaze/board.h
@@ -1,14 +1,23 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <stdbool.h>
 #include <stdint.h>
+
+enum { WHITE, BLACK, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
 
 /* main structure to hold the state of the board */
 struct board_t {
-        uint64_t piece[8];
+        uint64_t piece[8]; // piece bitboards
+        bool turn;         // side to move
+        uint8_t ply;       // half-move counter
+        uint8_t castle;    // castling rights
 };
 
 /* parse: parses `fen` (Forsyth–Edwards Notation) string into `board` structure */
 void parse(const char *fen, struct board_t *board);
+
+/* generate: gnerates pseudo-legal moves */
+void generate(const struct board_t *board, uint16_t *moves);
 
 #endif /* BOARD_H */
