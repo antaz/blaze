8fa7c2c6ccb2770d24ab09093387aeb3d4f9a745
Antar Azri <azantar@proton.me>
2024-07-18 18:52:52 +0100

    UCI: Implement `position` command

--- src/blaze.c
+++ src/blaze.c
@@ -11,10 +11,5 @@ int main()
 {
     struct board_t board = {
         .bb = {0}, .turn = BLACK, .ep = NOSQ, .ca = 0, .hist = {0}};
-    const char *start =
-        "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -";
-
-    parse(start, &board);
     loop(&board);
-
 }
--- src/uci.c
+++ src/uci.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
-#include <string.h>
 #include "uci.h"
 #include "board.h"
 #include "perft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define SIZE 1024
 
-void loop(struct board_t *board) {
+void loop(struct board_t *board)
+{
     char buf[1024];
 
     while (1) {
-        if(fgets(buf, 1024, stdin) != NULL){
-            if(strcmp("uci\n", buf) == 0) {
+        if (fgets(buf, 1024, stdin) != NULL) {
+            if (strcmp("uci\n", buf) == 0) {
                 printf("uciok\n");
-            } else if(strcmp("ready\n", buf) == 0) {
+            } else if (strcmp("ready\n", buf) == 0) {
                 printf("readyok\n");
-            } else if(strncmp("go perft", buf, 7) == 0) {
-                char depth;
-                sscanf(buf, "go perft %c", &depth);
-                perft_divide(board, depth - '0');
+            } else if (strncmp("go perft", buf, 7) == 0) {
+                perft_divide(board, atoi(buf + 9));
+            } else if (strncmp("position startpos", buf, 16) == 0) {
+                parse(
+                    "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
+                    board);
+            } else if (strncmp("position fen", buf, 11) == 0) {
+                parse(buf + 13, board);
             }
         }
     }
