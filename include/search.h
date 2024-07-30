450d99da34c1c614ad6081f740cf69542e634d72
Antar Azri <azantar@proton.me>
2024-07-30 17:03:48 +0100

    Add chess clock structure

--- include/search.h
+++ include/search.h
@@ -1,6 +1,20 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
-void search(struct board_t *board);
+#include "board.h"
+
+#define MOVESTOGO 24
+#define MAX_DEPTH 64
+
+struct clck_t {
+    int time[2];
+    int inc[2];
+    int depth;
+    int nodes;
+    int movetime;
+    int movestogo;
+};
+
+void search(struct board_t *board, struct clck_t *clock);
 
 #endif
