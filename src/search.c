8ca8425ff5d4ad2cf34213e10c7a63344f802008
Antar Azri <azantar@proton.me>
2024-07-30 17:04:46 +0100

    Add rudimentary time management

--- src/search.c
+++ src/search.c
@@ -1,22 +1,42 @@
+#include "search.h"
 #include "board.h"
 #include "gen.h"
 #include "move.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 
-void search(struct board_t *board) {
+uint64_t time_ms()
+{
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (uint64_t)ts.tv_sec * 1e3 + ts.tv_nsec / 1e6;
+}
+
+void search(struct board_t *board, struct clck_t *clock)
+{
+
+    // init move and search time
+    uint64_t start = time_ms();
+    int movetime = 0;
+    if (clock->movetime != 0) {
+        movetime = clock->movetime;
+    } else {
+        movetime = clock->time[board->turn] / clock->movestogo +
+                   clock->inc[board->turn];
+    }
+
+    // generate legal moves
     uint16_t moves[256];
-    int count = 0;
+    gen_legal(board, moves);
 
-    count = gen_legal(board, moves);
+    for (;;) {
 
-    // initialize srand
-    srand(time(NULL));
+        // if time is over return bestmove and break;
+        if ((int)(time_ms() - start) > movetime) {
+            break;
+        }
+    }
 
-    int idx = rand() % count;
-    char *bestmove = str_move(moves[idx], board->turn);
-
-    // send the first move
-    printf("bestmove %s\n", bestmove);
+    printf("bestmove %s\n", str_move(moves[0], board->turn));
 }
