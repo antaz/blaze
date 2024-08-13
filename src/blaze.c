76f1d1da1b6e3bf2d5013852b7eebf5353090e97
Antar Azri <azantar@proton.me>
2024-08-13 17:05:49 +0100

    Init prehash tables and fix function call

--- src/blaze.c
+++ src/blaze.c
@@ -1,11 +1,13 @@
 #include "board.h"
+#include "hash.h"
 #include "uci.h"
 
 int main()
 {
+    init_hash();
     struct board_t board = {
-        .bb = {0}, .turn = WHITE, .ep = NOSQ, .ca = 0, .hist = {0}};
-    parse("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", &board);
+        .bb = {0}, .stm = WHITE, .ep = NOSQ, .ca = 0, .hist = {0}};
+    parse(&board, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     loop(&board);
     return 0;
 }
